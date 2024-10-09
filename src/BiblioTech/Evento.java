a5b37b369705de715c6ba668d50bf81d96924652
Markel Mencía <markel.mencia@opendeusto.es>
2024-10-09 10:43:46 +0200

    ➕ Add Evento attributes

--- src/BiblioTech/Evento.java
+++ src/BiblioTech/Evento.java
@@ -1,9 +1,67 @@
 package BiblioTech;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Evento {
 
-	public Evento() {
-		// TODO Auto-generated constructor stub
+	private TipoEvento tipoEvento;
+	private ArrayList<Cliente> asistentes;
+	private SalaEventos sala;
+	
+	public TipoEvento getTipoEvento() {
+		return tipoEvento;	
 	}
 
+	public void setTipoEvento(TipoEvento tipoEvento) {
+		this.tipoEvento = tipoEvento;
+	}
+	
+	public ArrayList<Cliente> getAsistentes() {
+		return asistentes;
+	}
+	
+	public void setAsistentes(ArrayList<Cliente> asistentes) {
+		this.asistentes = asistentes;
+	}
+	
+	public SalaEventos getSala() {
+		return sala;
+	}
+	
+	public void setSala(SalaEventos sala) {
+		this.sala = sala;
+	}
+	
+	public Evento(TipoEvento tipoEvento, ArrayList<Cliente> asistentes, SalaEventos sala) {
+		super();
+		this.tipoEvento = tipoEvento;
+		this.asistentes = asistentes;
+		this.sala = sala;
+	}
+	
+	public Evento() {
+		super();
+		this.tipoEvento = null;
+		this.asistentes = new ArrayList<>();
+		this.sala = new SalaEventos();
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evento other = (Evento) obj;
+		return Objects.equals(asistentes, other.asistentes) && Objects.equals(sala, other.sala)
+				&& tipoEvento == other.tipoEvento;
+	}
+
+	@Override
+	public String toString() {
+		return "Evento [tipoEvento=" + tipoEvento + ", asistentes=" + asistentes + ", sala=" + sala + "]";
+	}
 }
