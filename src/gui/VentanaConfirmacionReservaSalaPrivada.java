590587daed223de75ea460742b319ff7537b706f
aroa-im <aroa.i.m@opendeusto.es>
2024-11-02 12:27:50 +0100

    🧟‍♀️refactorizar código
    Hacer dos metodos getPanel, getJtextField

--- src/gui/VentanaConfirmacionReservaSalaPrivada.java
+++ src/gui/VentanaConfirmacionReservaSalaPrivada.java
@@ -58,28 +58,13 @@ public class VentanaConfirmacionReservaSalaPrivada extends JFrame{
 		titulo.add(topText);
 		
 		//panel fecha
-        JPanel panelFecha = new JPanel();
-        JLabel fecha = new JLabel("Fecha:");
-        JTextField textoFecha = new JTextField();
-        textoFecha.setColumns(10);
-        panelFecha.add(fecha);
-        panelFecha.add(textoFecha);
+        JPanel panelFecha = getPanel("Fecha:", "");
         
         //panel hora de entrada
-      	JPanel panelHoraEntrada = new JPanel();
-        JLabel horaEntrada = new JLabel("Hora de Entrada:");
-        JTextField textoHoraEntrada = new JTextField();
-        textoHoraEntrada.setColumns(10);
-        panelHoraEntrada.add(horaEntrada);
-        panelHoraEntrada.add(textoHoraEntrada);
+      	JPanel panelHoraEntrada = getPanel("Hora de Entrada:", "");
         
       	//panel hora de salida
-      	JPanel panelHoraSalida = new JPanel();
-        JLabel horaSalida = new JLabel("Hora de Salida:");
-        JTextField textoHoraSalida = new JTextField();
-        textoHoraSalida.setColumns(10);
-        panelHoraSalida.add(horaSalida);
-        panelHoraSalida.add(textoHoraSalida);
+      	JPanel panelHoraSalida = getPanel("Hora de Salida:", "");
         
         //panel boton
         JPanel panelBoton = new JPanel();
@@ -101,7 +86,97 @@ public class VentanaConfirmacionReservaSalaPrivada extends JFrame{
       
 		setVisible(true);
 	}
+	
+	private JPanel getPanel(String labelText, String placeholder) {
+		// Crear un panel con FlowLayout alineado a la izquierda
+        JPanel panelContenidoCentrado = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        // Panel principal que contiene el nombre y se organiza en Y_AXIS
+        JPanel panelContenido = new JPanel();
+        panelContenido.setLayout(new BoxLayout(panelContenido, BoxLayout.Y_AXIS));
+
+        // Crear el JLabel "Nombre" con alineación a la izquierda
+        JLabel label = new JLabel(labelText);
+        label.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        // Panel para el JLabel, también alineado a la izquierda
+        JPanel panelLabel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        panelLabel.add(label);
+
+        // Panel para el JTextField y el icono, alineados a la izquierda
+        JPanel panelInput = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JTextField input = (JTextField) getJTextField(labelText);
+        //input.setEnabled(false);
+        
+        panelInput.add(input);
+        
+        // Agregar los subpaneles al panel principal
+        panelContenido.add(panelLabel);
+        panelContenido.add(panelInput);
+
+        // Agregar el panel principal al panel central con alineación a la izquierda
+        panelContenidoCentrado.add(panelContenido);
+        
+        return panelContenidoCentrado;
+	}
+	
+	private JComponent getJTextField(String labelText) {
+		if (labelText.equalsIgnoreCase("Fecha:")) {
+			MaskFormatter formateadorMascara = null;
+	        try {
+	           formateadorMascara = new MaskFormatter("##/##/####") {
+
+	                // este método convierte la cadena de texto que introduce el usuario
+	                // en un objeto Date que será devuelto por el campo de texto cuando
+	                // se llame al método getValue
+	                @Override
+	                public Object stringToValue(String text) throws ParseException {
+
+	                    if (text == null || text.length() == 0) {
+	                        return null;
+	                    }
+	                    // Validar que el texto que introduce el usuario tenga 10 caracteres
+	                    if (text.length() != 10) {
+	                        throw new ParseException("Formato dd/MM/yyyy", 0);
+	                    }
+
+	                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	                    sdf.setLenient(false); // no admite fechas inválidas
+	                    return sdf.parse(text);
+	                }
+
+	                // este método convierte el objeto Date que maneja el campo de texto
+	                // en una cadena con el formato dd/MM/yyyy
+	                @Override
+	                public String valueToString(Object value) throws ParseException {
+	                    if (value == null || !(value instanceof Date)) {
+	                        return super.valueToString(value);
+	                    }
+	                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	                    return sdf.format(value);
+	                }
+
+	            };
+	        } catch (ParseException e) {
+	            System.out.println("Error en el formateador de fecha");
+	            e.printStackTrace();
+	        }
+
+	        formateadorMascara.setPlaceholderCharacter('_'); // caracter de relleno
+	        formateadorMascara.setAllowsInvalid(false); // no admite caracteres inválidos
+	        formateadorMascara.setOverwriteMode(true); // sobreescribe el texto si es inválido
+
+	        JFormattedTextField txtFieldFecha = new JFormattedTextField(formateadorMascara);
+	        txtFieldFecha.setColumns(15);
+	        
+	        return txtFieldFecha;
+			
+		} else {
+			return new JTextField("", 15);
+		}
 		
+	}
+	
 	public static void main(String[] args) {
 		VentanaConfirmacionReservaSalaPrivada ventana = new VentanaConfirmacionReservaSalaPrivada();
 	}
