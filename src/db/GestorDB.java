24d27061c3039f4ac0ca2920a665bb937ade8cb7
Markel Mencía <markel.mencia@opendeusto.es>
2024-12-01 14:25:33 +0100

    ⚒️ DB method files prepared

---
+++ src/db/GestorDB.java
@@ -0,0 +1,10 @@
+package db;
+
+import java.sql.Connection;
+
+public class GestorDB {
+
+    public static void consulta(Connection conn) {
+        
+    }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -2,17 +2,36 @@ package main;
 
 import BiblioTech.SalaPublica;
 import gui.Portada;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
 
     private static SalaPublica salaPublica = new SalaPublica(250, 0, 1, new ArrayList<>());
+    private static Connection connection;
 
     public static SalaPublica getSalaPublica() {
         return salaPublica;
     }
 
     public static void main(String[] args) {
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            } catch (ClassNotFoundException e) {
+            System.out.println("No se ha podido cargar el driver de la base de datos");
+            }
+
+        try {
+            connection = DriverManager.getConnection("jdbc:resources/db/bibliotech.db");
+        } catch (SQLException ex) {
+            System.err.println("ERROR: Base de datos no encontrada");
+        }
+
+        //consultas
+
         new Portada(null);
     }
 }
