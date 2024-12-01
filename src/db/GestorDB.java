ac27b80f6164a74855120866386fb816b82bc135
Sergitxin22 <sergio.morales@opendeusto.es>
2024-12-01 18:46:00 +0100

    ➕ Create Usuarios into db

--- src/db/GestorDB.java
+++ src/db/GestorDB.java
@@ -1,10 +1,84 @@
 package db;
 
+import java.io.File;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.PropertyPermission;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import BiblioTech.Cliente;
+import BiblioTech.Usuario;
 
 public class GestorDB {
 
-    public static void consulta(Connection conn) {
-        
+    private Connection conexionBD = null;
+    private Statement sentenciaBD = null;
+    private Logger logger = null;
+
+    public Connection init(String nombreBD) {
+        nombreBD = "resources/db/" + nombreBD;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection con = DriverManager.getConnection("jdbc:sqlite:" + nombreBD + ".db");
+            logger = Logger.getLogger("GestorPersistencia-" + nombreBD);
+            conexionBD = con;
+            return con;
+        } catch (ClassNotFoundException | SQLException | NullPointerException e) {
+            conexionBD = null;
+            if (logger != null)
+                logger.log(Level.SEVERE, "Error en conexión de base de datos " + nombreBD + ".db", e);
+            return null;
+        }
+    }
+
+    public boolean existeBD(String nombreBD) {
+        nombreBD = "resources/db/" + nombreBD;
+        File fichero = new File(nombreBD + ".db");
+        return fichero.exists() && fichero.length() > 0;
+    }
+
+    public Connection getConnection() {
+        return conexionBD;
+    }
+
+    public boolean addUsuario(Usuario usuario) {
+        try {
+            String insertSQL = "INSERT INTO Usuario VALUES (?,?,?,?,?)";
+            PreparedStatement preparedStmt = conexionBD.prepareStatement(insertSQL);
+            preparedStmt.setString(1, usuario.getDni());
+            preparedStmt.setString(2, usuario.getNombre());
+            preparedStmt.setString(3, usuario.getEmail());
+            preparedStmt.setString(4, usuario.getFechaCreacion().toString());
+            preparedStmt.setString(5, usuario.getContrasena());
+
+            int filas = preparedStmt.executeUpdate();
+            System.out.println("Filas modificadas: " + filas);
+
+            PreparedStatement preparedStmt2 = null;
+            if (usuario instanceof Cliente) {
+                String insertSQL2 = "INSERT INTO Cliente VALUES (?,?)";
+                preparedStmt2 = conexionBD.prepareStatement(insertSQL2);
+                preparedStmt2.setString(1, usuario.getDni());
+                preparedStmt2.setInt(2, ((Cliente) usuario).getAmonestaciones());
+            } else {
+                String insertSQL2 = "INSERT INTO Admin VALUES (?)";
+                preparedStmt2 = conexionBD.prepareStatement(insertSQL2);
+                preparedStmt2.setString(1, usuario.getDni());
+            }
+
+            int filas2 = preparedStmt2.executeUpdate();
+
+            preparedStmt.close();
+            preparedStmt2.close();
+            return (filas > 0 && filas2 > 0) ? true : false;
+        } catch (SQLException e) {
+            if (logger != null)
+                logger.log(Level.SEVERE, "Error en añadido de usuario: ", e);
+            return false;
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,37 +1,55 @@
 package main;
 
+import BiblioTech.Admin;
+import BiblioTech.Cliente;
 import BiblioTech.SalaPublica;
+import BiblioTech.Usuario;
+import db.GestorDB;
 import gui.Portada;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Main {
+    private static GestorDB bd = new GestorDB();
+    // private static SalaPublica salaPublica = new SalaPublica(250, 0, 1, new
+    // ArrayList<>());
 
-    private static SalaPublica salaPublica = new SalaPublica(250, 0, 1, new ArrayList<>());
-    private static Connection connection;
-
-    public static SalaPublica getSalaPublica() {
-        return salaPublica;
-    }
+    // public static SalaPublica getSalaPublica() {
+    // return salaPublica;
+    // }
 
     public static void main(String[] args) {
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            } catch (ClassNotFoundException e) {
-            System.out.println("No se ha podido cargar el driver de la base de datos");
-            }
+        // try {
+        // Class.forName("org.sqlite.JDBC");
+        // } catch (ClassNotFoundException e) {
+        // System.out.println("No se ha podido cargar el driver de la base de datos");
+        // }
+
+        // try {
+        // connection = DriverManager.getConnection("jdbc:resources/db/bibliotech.db");
+        // } catch (SQLException ex) {
+        // System.err.println("ERROR: Base de datos no encontrada");
+        // }
+
+        // //consultas
+        bd.init("bibliotech");
+        System.out.println(bd.existeBD("bibliotech"));
+        Cliente u = new Cliente("00000000A", "Sergio", "sergio@si.es", LocalDateTime.now(), "hola", new ArrayList<>(),
+                new ArrayList<>(), 0);
+        Admin a = new Admin("11111111B", "Aroa", "aroa@no.com", LocalDateTime.now(), "aroa2003", new ArrayList<>());
+
+        bd.addUsuario(u);
+        bd.addUsuario(a);
 
         try {
-            connection = DriverManager.getConnection("jdbc:resources/db/bibliotech.db");
-        } catch (SQLException ex) {
-            System.err.println("ERROR: Base de datos no encontrada");
+            bd.getConnection().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-        //consultas
-
-        new Portada(null);
+        // new Portada(null);
     }
 }
