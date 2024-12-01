6ac1a8ff467eeba9da227eb5ce44b2a7497f5b9d
Markel Mencía <markel.mencia@opendeusto.es>
2024-12-01 12:31:40 +0100

    ✔️ Finish functionality for SalaPublica

--- src/BiblioTech/SalaPublica.java
+++ src/BiblioTech/SalaPublica.java
@@ -1,19 +1,33 @@
 package BiblioTech;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SalaPublica extends Sala {
 	private ArrayList<Admin> supervisores;
+	private HashMap<Integer, Cliente> clientesPorBloque;
 	
-	public SalaPublica(int capacidad, int iD, int piso, ArrayList<Cliente> listaClientes,
-			ArrayList<Admin> supervisores) {
+	public SalaPublica(int capacidad, int iD, int piso, ArrayList<Cliente> listaClientes) {
 		super(capacidad, iD, piso, listaClientes);
-		this.supervisores = supervisores;
+
+		this.supervisores = new ArrayList<>();
+		HashMap<Integer, Cliente> mapa = new HashMap<>();
+		for (int i = 1; i < 251; i++) {
+			mapa.put(i, null);
+		}
+
+		this.clientesPorBloque = mapa;
 	}
 	
 	public SalaPublica() {
 		super();
-		this.supervisores = new ArrayList<Admin>();
+		HashMap<Integer, Cliente> mapa = new HashMap<>();
+		for (int i = 1; i < 251; i++) {
+			mapa.put(i, null);
+		}
+
+		this.supervisores = new ArrayList<>();
+		this.clientesPorBloque = mapa;
 	}
 
 	public ArrayList<Admin> getSupervisores() {
@@ -24,10 +38,18 @@ public class SalaPublica extends Sala {
 		this.supervisores = supervisores;
 	}
 
+	public HashMap<Integer, Cliente> getClientesPorBloque() {
+		return clientesPorBloque;
+	}
+
+	public void setClientesPorBloque(HashMap<Integer, Cliente> clientesPorBloque) {
+		this.clientesPorBloque = clientesPorBloque;
+	}
+
 	@Override
 	public String toString() {
 		return "SalaPublica [supervisores=" + supervisores + ", getCapacidad()=" + getCapacidad() + ", getId()="
-				+ getId() + ", getPiso()=" + getPiso() + ", getListaClientes()=" + getListaClientes() + "]";
+				+ getId() + ", getPiso()=" + getPiso() + ", getListaClientes()=" + getListaClientes() + ", getClientesPorBloque()=" + getClientesPorBloque() + "]";
 	}
 
 	
