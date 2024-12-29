3212dc5ca92c15f235632c90898df30a641ad804
Markel Mencía <markel.mencia@opendeusto.es>
2024-12-29 15:13:31 +0100

    🎁 Add ReviewDAO files

---
+++ src/dbmejorada/ReviewDAOInterface.java
@@ -0,0 +1,16 @@
+package dbmejorada;
+
+import java.util.ArrayList;
+
+import domain.Libro;
+import domain.Review;
+import domain.Usuario;
+
+
+public interface ReviewDAOInterface {
+	
+	boolean addReview(Review review);
+	ArrayList<Review> getReviewsUsuario(Usuario usuario);
+	ArrayList<Review> getReviewsLibro(Libro libro);
+	
+}
