395cf5f231e657ad13b990db879ea5e535af34ea
Oier Artabe <oier.artabe@opendeusto.es>
2025-01-04 21:19:06 +0100

    Arreglar Swing

--- src/gui/VentanaConfirmaciónDeReserva.java
+++ src/gui/VentanaConfirmaciónDeReserva.java
@@ -22,39 +22,71 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
 
 import domain.Cliente;
 import domain.Libro;
 import domain.Seccion;
+import domain.Usuario;
 import gui.components.Header;
+import main.Main;
 
 public class VentanaConfirmaciónDeReserva extends JFrame {
+	
+	private JPanel pOeste, pEste, pSur, pCentro, pHeader;
+	private Usuario usuario = Main.getUsuario();
 
 	private static final long serialVersionUID = -5490640345084381273L;
+	
 	public VentanaConfirmaciónDeReserva(Libro libro) {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setTitle("Confirmación de reserva");
 		setSize(1280, 720);
 		setLocationRelativeTo(null);
 		
+		
+		getContentPane().setBackground(Color.WHITE);
+		
+		pCentro = new JPanel();
+	    pSur = new JPanel();
+	    pEste = new JPanel();
+	    pOeste = new JPanel();
+	    pHeader = new Header(Seccion.BIBLIOTECA, usuario, this);
+	  
+	    pCentro.setBackground(Color.WHITE);
+        pSur.setBackground(Color.WHITE);
+        pHeader.setBackground(Color.WHITE);
+        pOeste.setBackground(Color.WHITE);
+        pEste.setBackground(Color.WHITE);
+        
+	    getContentPane().add(pCentro, BorderLayout.CENTER);
+	    getContentPane().add(pHeader, BorderLayout.NORTH);
+		getContentPane().add(pSur, BorderLayout.SOUTH);
+		getContentPane().add(pEste, BorderLayout.EAST);
+		getContentPane().add(pOeste, BorderLayout.WEST);
+		
+		pCentro.setLayout(new BoxLayout(pCentro, BoxLayout.Y_AXIS));
+		pCentro.setBackground(Color.WHITE);
+		
 		//panel header
 		Header header = new Header(Seccion.BIBLIOTECA, new Cliente(), this);
 		header.setBackground(Color.WHITE);
-		//panel body
-		JPanel body = new JPanel(new GridLayout(0,2));
-		body.setBackground(Color.WHITE);
 		
-		//panel imagen
+		//panel de imagen
+		
 		JPanel panelimagenLibro= new JPanel();
 		panelimagenLibro.setBackground(Color.WHITE);
+		panelimagenLibro.setAlignmentX(CENTER_ALIGNMENT);
+		panelimagenLibro.setBorder(new EmptyBorder (0,65,0,0));
 		JLabel imagenDelLibro = new JLabel();
-		imagenDelLibro.setPreferredSize(new Dimension(550,700));
-		Image imagen = libro.getFoto().getImage().getScaledInstance(250, 400, Image.SCALE_SMOOTH);
+		imagenDelLibro.setPreferredSize(new Dimension(275,500));
+		Image imagen = libro.getFoto().getImage().getScaledInstance(200, 350, Image.SCALE_SMOOTH);
 		ImageIcon imagenEscalada = new ImageIcon(imagen);
 		imagenDelLibro.setIcon(imagenEscalada);
+		imagenDelLibro.setAlignmentX(CENTER_ALIGNMENT);
+		
 		panelimagenLibro.add(imagenDelLibro);
-		panelimagenLibro.setBackground(Color.WHITE);
-		body.add(panelimagenLibro);
+		pOeste.add(panelimagenLibro);	
 		
 		//panel texto
 		JPanel panelTexto = new JPanel();
@@ -63,6 +95,9 @@ public class VentanaConfirmaciónDeReserva extends JFrame {
 		JLabel labelTitulo = new JLabel(libro.getTitulo());
 		Font fuente = new Font("ARIAL",Font.BOLD, 32);
 		labelTitulo.setFont(fuente);
+		labelTitulo.setAlignmentX(CENTER_ALIGNMENT);
+		panelTexto.add(labelTitulo);
+		panelTexto.setBorder(new EmptyBorder(60,0,0,85));
 		
 		
 		List<JTextArea> areas = new ArrayList<>();
@@ -97,22 +132,26 @@ public class VentanaConfirmaciónDeReserva extends JFrame {
         descripcionScrollPane.setPreferredSize(new Dimension(600, 500)); // Tamaño más grande para descripción
         descripcionScrollPane.setBorder(null);
         
-        
+        pCentro.add(panelTexto);
+        pCentro.add(taAutor);
+        pCentro.add(taGenero);
+        pCentro.add(taNumeroPaginas);
+        pCentro.add(taRating);
+        pCentro.add(taSinopsis);
         
         //panel boton
-		JPanel botonesPanel = new JPanel(new GridBagLayout());
+		JPanel botonesPanel = new JPanel();
 		JButton botonConfirmar = new JButton("Confirmar reserva");
-		botonesPanel.setPreferredSize(new Dimension(100,100));
+		JButton botonVolver = new JButton ("Volver");
+
 		botonesPanel.setBackground(Color.WHITE);
-		
-		GridBagConstraints gbc = new GridBagConstraints();
-		//gbc.insets = new Insets(10, 10, 10, 10);
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.anchor = GridBagConstraints.CENTER;
-        
+        botonesPanel.add(botonVolver);
+        botonesPanel.add(botonConfirmar);
 		botonConfirmar.setFont(new Font("Arial", Font.BOLD, 17));
-		botonConfirmar.setPreferredSize(new Dimension(200, 50));
+		botonVolver.setFont(new Font("Arial", Font.BOLD, 17));
+		botonesPanel.setBorder(new EmptyBorder(0,0,50,0));
+		
+		pCentro.add(botonesPanel);
 		
 		botonConfirmar.addActionListener(new ActionListener() {
 
@@ -123,27 +162,36 @@ public class VentanaConfirmaciónDeReserva extends JFrame {
 			}
 		});
 		
+		botonVolver.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaInformacionRecurso nuevaVentana = new VentanaInformacionRecurso(libro);
+				nuevaVentana.setVisible(true);
+				dispose();
+			}
+			
+		});
+		
 		add(header,BorderLayout.NORTH);
-		add(body);
+		/*add(body, BorderLayout.CENTER);
 		
 		body.add(labelTitulo, BorderLayout.NORTH);
         body.add(labelTitulo);
         body.add(panelTexto);
 		body.add(panelimagenLibro, BorderLayout.EAST);
 		body.add(panelTexto, BorderLayout.WEST);
-		body.add(botonConfirmar);
 		
-		panelimagenLibro.setLayout(new GridBagLayout());
-		panelimagenLibro.add(imagenDelLibro,gbc);
+		panelimagenLibro.add(imagenDelLibro);
 		
-		panelTexto.add(labelTitulo,gbc);
+		panelTexto.add(labelTitulo);
 		panelTexto.add(taAutor, BorderLayout.CENTER);
         panelTexto.add(taGenero, BorderLayout.CENTER);
         panelTexto.add(taNumeroPaginas, BorderLayout.CENTER);
         panelTexto.add(taRating, BorderLayout.CENTER);
-		panelTexto.add(descripcionScrollPane,gbc);
-		panelTexto.add(botonConfirmar, gbc);
-		
+		panelTexto.add(descripcionScrollPane);
+		panelTexto.add(botonesPanel);
+		*/
 		setVisible(true);
 	} 
 	public static void main(String[] args) {
