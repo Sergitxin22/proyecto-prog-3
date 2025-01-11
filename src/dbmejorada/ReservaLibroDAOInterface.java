361e06fda4f64867e47e4b7e91df3e10c4902ea0
aroa-im <aroa.i.m@opendeusto.es>
2025-01-11 21:26:12 +0100

    🍪 Create reservaSalaDAOInterface

---
+++ src/dbmejorada/ReservaLibroDAOInterface.java
@@ -0,0 +1,14 @@
+package dbmejorada;
+
+import java.util.ArrayList;
+
+public interface ReservaLibroDAOInterface {
+	boolean addReservaLibro(ReservaLibroDTO reserva);
+	ReservaLibroDTO getReservaLibroById(int idReservaSala);
+	ArrayList<ReservaLibroDTO> getHistorialByDniUsuarioDTO(String dniUsuario);
+	boolean deleteReservaLibroById(int idReservaSala);
+	boolean isLibroDisponible(ReservaLibroDTO reserva); 
+	boolean libroLeidoByUsuarioDni(String dniCliente, Long isbnLibro);
+	boolean updateReservaLibro(ReservaLibroDTO reserva);
+	void borrarRegistros();
+}
